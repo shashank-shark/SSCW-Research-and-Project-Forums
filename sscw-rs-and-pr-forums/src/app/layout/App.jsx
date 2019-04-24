7a939f21655952db9c10436322ef7ac4c78828d4
Shashank J <shashankreddy231@gmail.com>
2019-04-24 22:58:21 +0530

    Added navbar component.

---
+++ sscw-rs-and-pr-forums/src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import NavBarLogin from '../../features/nav/NavBarLogin/NavBarLogin'
+
+class App extends Component {
+  render() {
+    return (
+      <NavBarLogin />
+    )
+  }
+}
+
+
+export default App;
