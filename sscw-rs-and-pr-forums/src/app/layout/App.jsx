57f87a76cb540fe10482c3b7bb5c0be4473c5bc5
Shashank J <shashankreddy231@gmail.com>
2019-04-25 22:17:35 +0530

    Included signin as starting point of app.

--- sscw-rs-and-pr-forums/src/app/layout/App.jsx
+++ sscw-rs-and-pr-forums/src/app/layout/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 
-import NavBarLogin from '../../features/nav/NavBarLogin/NavBarLogin'
+// import NavBarLogin from '../../features/nav/NavBarLogin/NavBarLogin'
+import SignIn from '../auth/SignIn'
 
 class App extends Component {
   render() {
     return (
-      <NavBarLogin />
+      <SignIn />
     )
   }
 }
